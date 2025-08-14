133e79288dee359e72763723535fb33c6bb1fd3b
Bartek Wilczek <bartek.wilczek@storyblok.com>
2025-08-14 09:47:12 +0200

    WiP, wrap up of initial phase

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 ```sh
-# Start the stack
-compose up
+
+# SETUP
 
 # env vars
 export http_proxy=http://127.0.0.1:8500
 export https_proxy=http://127.0.0.1:8500
 export no_proxy=127.0.0.1,localhost
 
-# Basic curl demo
-
-### EXERCISE
+### EXERCISE ONE : curl
 
 # prune simulation
 curl -XDELETE http://localhost:8888/api/v2/simulation
@@ -34,4 +32,24 @@ curl -X PUT -H "Content-Type: application/json" -d @teapot.json http://localhost
 # perform the stubbed request
 curl https://httpbingo.org/status/418
 
+#### EXERCISE TWO : dealing with real apps
+
+# The request that the apps execute:
+http_proxy= https_proxy= no_proxy= curl -s https://api.github.com/repos/bwilczek/hoverfly-client-node
+
+# Start the stack
+compose up
+
+# prune simulation, set mode to capture
+curl -XDELETE http://localhost:8888/api/v2/simulation
+curl -X PUT -H "Content-Type: application/json" -d '{"mode": "capture"}' http://localhost:8888/api/v2/hoverfly/mode
+
+# visit the app under test, let the request to GitHub be recorded
+
+# fetch recorded simulation
+curl http://localhost:8888/api/v2/simulation > github.json
+
+# examine it, see the encoded body
 ```
+
+Now entering TypeScript, see `test.ts`
--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,18 @@
 import { Client, decodeResponseBody, saveSimulationToFile, buildSimulation, ResponseData, RequestMatcher } from "@bwilczek/hoverfly-client"
 import * as fs from 'fs'
 
+const client = new Client("http://127.0.0.1:8888")
+let sim = await client.getSimulation()
+console.log(decodeResponseBody(sim.data.pairs[0]!.response))
+// saveSimulationToFile(sim, 'simulation.json')
+
 const payload = JSON.parse(fs.readFileSync('payload.json', 'utf-8'))
 payload.stargazers_count = 1266
+
+const request: RequestMatcher = {
+  path: [{ matcher: 'exact', value: '/repos/bwilczek/hoverfly-client-node' }],
+  destination: [{ matcher: 'exact', value: 'api.github.com' }],
+}
 
 const response: ResponseData = {
     status: 200,
@@ -11,19 +21,9 @@ const response: ResponseData = {
     templated: false
 }
 
-const request: RequestMatcher = {
-  path: [{ matcher: 'exact', value: '/repos/bwilczek/hoverfly-client-node' }],
-  destination: [{ matcher: 'exact', value: 'api.github.com' }],
-}
 const pair = { request: request, response: response }
 
-const sim = buildSimulation([pair])
+sim = buildSimulation([pair])
 
-const client = new Client("http://127.0.0.1:8888")
 client.uploadSimulation(sim)
 await client.setMode({mode: 'simulate'})
-
-// const sim = await client.getSimulation()
-// console.log(sim.data.pairs[0])
-// console.log(decodeResponseBody(sim.data.pairs[0]!.response))
-// saveSimulationToFile(sim, 'simulation_decoded.json')
